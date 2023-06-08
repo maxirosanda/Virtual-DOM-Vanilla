525ec5930fc71fd5f7398e11e2226231f2a8e9fe
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-06-08 12:01:12 -0300

    2 files modified, 3 added, 1 deleted

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.jsx",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  },
+  devtool: "eval-source-map",
+};
